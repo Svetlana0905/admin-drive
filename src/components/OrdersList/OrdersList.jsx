0f1dadbc633a3a5aa0c65b489181080a43706814
Svetlana <pokrowsckaya.sveta@gmail.com>
2022-06-01 12:23:25 +0300

    V-08 разбивка на компоненты OrdersList

--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -1,6 +1,7 @@
-import stub from '../../assets/stub.jpg'
-import { Checkbox } from '../../components/Button/Button'
-import { getDate } from '../../components/GetDate/GetDate'
+// import { Checkbox } from '../../components/Button/Button'
+import { OrderContentColumn } from '../OrderContentColumn/OrderContentColumn'
+import { CarImageBlock } from '../CarImageBlock/CarImageBlock'
+import { CheckBoxReadOnly } from '../CheckBoxReadOnlyBlock/CheckBoxReadOnly'
 
 export const OrdersList = ({
   data,
@@ -14,61 +15,10 @@ export const OrdersList = ({
         data.data.map((item) => (
           <div key={item.id} className="content__row">
             <div className="content__description">
-              <p className="content__img-inner">
-                {item.carId ? (
-                  <img
-                    src={item.carId.thumbnail.path}
-                    className="content__car-pic"
-                    alt={item.name}
-                  />
-                ) : (
-                  <img
-                    src={stub}
-                    className="content__car-pic"
-                    alt={item.name}
-                  />
-                )}
-              </p>
-              <div className="content__column">
-                <p className="text-dark">
-                  {item.carId ? item.carId.name : 'Авто не указан'}
-                  <span className="text-light"> в </span>
-                  <span className="text-dark">
-                    {item.cityId ? item.cityId.name : 'не указан'}
-                  </span>
-                  <span className="text-light">
-                    , {item.pointId ? item.pointId.address : 'не указан'}
-                  </span>
-                </p>
-                <p className="text-light">
-                  {getDate(item.dateFrom)} - {getDate(item.dateTo)}
-                </p>
-                <p className="text-dark">
-                  <span className="text-light">Цвет </span> {item.color}
-                </p>
-                <p className="text-dark">
-                  <span className="text-light">Статус </span>
-                  {item.orderStatusId ? item.orderStatusId.name : 'не указан'}
-                </p>
-              </div>
+              <CarImageBlock item={item} />
+              <OrderContentColumn item={item} />
             </div>
-            <div className="content__options">
-              <Checkbox
-                text={'Детское кресло'}
-                checked={item.isNeedChildChair}
-                readonly
-              />
-              <Checkbox
-                text={'Правый руль'}
-                checked={item.isRightWheel}
-                readonly
-              />
-              <Checkbox
-                text={'Полный бак'}
-                checked={item.isFullTank}
-                readonly
-              />
-            </div>
+            <CheckBoxReadOnly item={item} />
             <p className="content__price">
               {item.price ? item.price : 0}
               <span className="content__price"> ₽</span>
